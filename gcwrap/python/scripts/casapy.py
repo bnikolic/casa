aa30dfe326a48d771e97eead946418ffca2d9dce
Bojan Nikolic <b.nikolic@mrao.cam.ac.uk>
2016-12-28 16:34:06 +0000

    Apply casanix patches

--- gcwrap/python/scripts/casapy.py
+++ gcwrap/python/scripts/casapy.py
@@ -156,12 +156,12 @@ casa = { 'build': {
          },
          'dirs': {
              'rc': homedir + '/.casa',
-             'data': None,
+             'data': "@CASADATAROOT@/share/casa-data/",
              'recipes': None,
-             'root': None,
-             'python': None,
+             'root': '@CASAGCWRAPROOT@',
+             'python':  '@CASAGCWRAPROOT@/python/2.7/',
              'pipeline': None,
-             'xml': None
+             'xml': '@CASAGCWRAPROOT@/xml/'
          },
          'flags': { },
          'files': { 
@@ -178,58 +178,8 @@ casa = { 'build': {
 ## ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 ## set up casa root
 ## ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
-if os.environ.has_key('CASAPATH') :
-    __casapath__ = os.environ['CASAPATH'].split(' ')[0]
-    __casaarch__ = os.environ['CASAPATH'].split(' ')[1]
-    if not os.path.exists(__casapath__ + "/data") :
-        print "DEBUG: CASAPATH = %s" % (__casapath__)
-        raise RuntimeError, "Unable to find the data repository directory in your CASAPATH. Please fix."
-    else :
-        casa['dirs']['root'] = __casapath__
-        casa['dirs']['data'] = __casapath__ + "/data"
-        if os.path.exists(__casapath__ + "/" + __casaarch__ + "/lib/python2.7/assignmentFilter.py"):
-            casa['dirs']['python'] = __casapath__ + "/" + __casaarch__ + "/lib/python2.7"
-        elif os.path.exists(__casapath__ + "/lib/python2.7/assignmentFilter.py"):
-            casa['dirs']['python'] = __casapath__ + "/lib/python2.7"
-        elif os.path.exists(__casapath__ + "/Resources/python/assignmentFilter.py"):
-            casa['dirs']['python'] = __casapath__ + "/Resources/python"
-
-        if casa['dirs']['python'] is not None:
-            casa['dirs']['recipes'] = casa['dirs']['python'] + "/recipes"
-
-        if os.path.exists(__casapath__ + "/" + __casaarch__ + "/xml"):
-            casa['dirs']['xml'] = __casapath__ + "/" + __casaarch__ + "/xml"
-        elif os.path.exists(__casapath__ + "/xml"):
-            casa['dirs']['xml'] = __casapath__ + "/xml"
-        else:
-            raise RuntimeError, "Unable to find the XML constraints directory in your CASAPATH"
-else :
-    __casapath__ = casac.__file__
-    while __casapath__ and __casapath__ != "/" :
-        if os.path.exists( __casapath__ + "/data") :
-            break
-        __casapath__ = os.path.dirname(__casapath__)
-    if not os.path.exists(__casapath__ + "/data") :
-        raise RuntimeError, "casa path could not be determined"
-    else :
-        casa['dirs']['root'] = __casapath__
-        casa['dirs']['data'] = __casapath__ + "/data"
-        if os.path.exists(__casapath__ + "/" + __casaarch__ + "/lib/python2.7/assignmentFilter.py"):
-            casa['dirs']['python'] = __casapath__ + "/" + __casaarch__ + "/lib/python2.7"
-        elif os.path.exists(__casapath__ + "/lib/python2.7/assignmentFilter.py"):
-            casa['dirs']['python'] = __casapath__ + "/lib/python2.7"
-        elif os.path.exists(__casapath__ + "/Resources/python/assignmentFilter.py"):
-            casa['dirs']['python'] = __casapath__ + "/Resources/python"
-
-        if casa['dirs']['python'] is not None:
-            casa['dirs']['recipes'] = casa['dirs']['python'] + "/recipes"
-
-        if os.path.exists(__casapath__ + "/" + __casaarch__ + "/xml"):
-            casa['dirs']['xml'] = __casapath__ + "/" + __casaarch__ + "/xml"
-        elif os.path.exists(__casapath__ + "/xml"):
-            casa['dirs']['xml'] = __casapath__ + "/xml"
-        else:
-            raise RuntimeError, "Unable to find the XML constraints directory in your CASAPATH"
+__casapath__ = "/"
+__casaarch__ = "linux"
 
 
 ## ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
@@ -782,22 +732,13 @@ def update_params(func, printtext=True, ipython_globals=None):
 	if(has_othertasks) :
 	   has_task = myf['task_location'].has_key(myf['taskname'])
 	   if (has_task) :
-		pathname=myf['task_location'][myf['taskname']]
+               pathname=myf['task_location'][myf['taskname']]
 	   else :
-	        pathname = os.environ.get('CASAPATH').split()[0]+'/'+os.environ.get('CASAPATH').split()[1]+'/xml'
-                if not os.path.exists(pathname) :
-                    pathname = os.environ.get('CASAPATH').split()[0]+'/xml'
-                    if not os.path.exists(pathname) :
-                        pathname = os.environ.get('CASAPATH').split()[0]+'/Resources/xml'
-                
+               pathname = casa['dirs']['xml']
 	else :
-	   pathname = os.environ.get('CASAPATH').split()[0]+'/'+os.environ.get('CASAPATH').split()[1]+'/xml'
-           if not os.path.exists(pathname) :
-               pathname = os.environ.get('CASAPATH').split()[0]+'/xml'
-               if not os.path.exists(pathname) :
-                   pathname = os.environ.get('CASAPATH').split()[0]+'/Resources/xml'
-                   if not os.path.exists(pathname) :
-                       sys.exit("ERROR: casapy.py update_params() can not locate xml file for task %s" % (taskname))
+            pathname = casa['dirs']['xml']
+            if not os.path.exists(pathname):
+                sys.exit("ERROR: casapy.py update_params() can not locate xml file for task %s" % (taskname))
                        
         xmlfile=pathname+'/'+myf['taskname']+'.xml'
         if(os.path.exists(xmlfile)) :
@@ -1353,8 +1294,6 @@ from task_help import *
 #
 # import testing environment
 #
-import publish_summary
-import runUnitTest
 #
 home=os.environ['HOME']
 
@@ -1437,7 +1376,8 @@ if casa['flags'].has_key('-c') :
             ###
             try:
                 ipshell = IPython.Shell.IPShell( argv=['-prompt_in1','CASA <\#>: ','-autocall','2','-colors',__ipython_colors, '-nomessages', '-nobanner','-logfile',ipythonlog,'-ipythondir',casa['dirs']['rc']+'/ipython','-c','execfile("'+casa['flags']['-c']+'")'], user_ns=globals() )
-            except:
+            except e:
+                print e
                 print "ERROR: falied to create an instance of IPython.Shell.IPShell"
         else:
             ###
@@ -1445,7 +1385,8 @@ if casa['flags'].has_key('-c') :
             ###
             try:
                 ipshell = IPython.Shell.IPShell( argv=['-prompt_in1','CASA <\#>: ','-autocall','2','-colors',__ipython_colors, '-nomessages', '-nobanner','-logfile',ipythonlog,'-ipythondir',casa['dirs']['rc']+'/ipython','-c',casa['flags']['-c']], user_ns=globals() )
-            except: 
+            except e:
+                print e
                 print "ERROR: falied to create an instance of IPython.Shell.IPShell"
     else:
         if os.path.exists( casa['flags']['-c'] ) :
@@ -1454,7 +1395,8 @@ if casa['flags'].has_key('-c') :
             ###
             try:
                 ipshell = IPython.Shell.IPShell( argv=['-prompt_in1','CASA <\#>: ','-autocall','2','-colors',__ipython_colors, '-nomessages', '-nobanner','-logfile',ipythonlog,'-upgrade','-ipythondir',casa['dirs']['rc']+'/ipython','-c','execfile("'+casa['flags']['-c']+'")'], user_ns=globals() )
-            except: 
+            except e:
+                print e
                 print "ERROR: falied to create an instance of IPython.Shell.IPShell"
         else:
             ###
@@ -1462,30 +1404,35 @@ if casa['flags'].has_key('-c') :
             ###
             try:
                 ipshell = IPython.Shell.IPShell( argv=['-prompt_in1','CASA <\#>: ','-autocall','2','-colors',__ipython_colors, '-nomessages', '-nobanner','-logfile',ipythonlog,'-upgrade','-ipythondir',casa['dirs']['rc']+'/ipython','-c',casa['flags']['-c']], user_ns=globals() )
-            except: 
+            except e:
+                print e
                 print "ERROR: falied to create an instance of IPython.Shell.IPShell"
 else:
     if os.path.exists( casa['dirs']['rc']+'/ipython/ipy_user_conf.py' ) :
         if(thelogfile != 'null') :
             try:
                 ipshell = IPython.Shell.IPShell( argv=['-prompt_in1','CASA <\#>: ','-autocall','2','-colors',__ipython_colors, '-nomessages', '-nobanner','-logfile',ipythonlog,'-ipythondir',casa['dirs']['rc']+'/ipython'], user_ns=globals() )
-            except: 
+            except e:
+                print e
                 print "ERROR: falied to create an instance of IPython.Shell.IPShell"
         else :
             try:
                 ipshell = IPython.Shell.IPShell( argv=['-prompt_in1','CASA <\#>: ','-autocall','2','-colors',__ipython_colors, '-nomessages', '-nobanner','-ipythondir',casa['dirs']['rc']+'/ipython'], user_ns=globals() )
-            except: 
+            except e:
+                print e
                 print "ERROR: falied to create an instance of IPython.Shell.IPShell"
     else:
         if(thelogfile != 'null') :
             try:
                 ipshell = IPython.Shell.IPShell( argv=['-prompt_in1','CASA <\#>: ','-autocall','2','-colors',__ipython_colors, '-nomessages', '-nobanner','-logfile',ipythonlog,'-upgrade','-ipythondir',casa['dirs']['rc']+'/ipython'], user_ns=globals() )
-            except: 
+            except e:
+                print e
                 print "ERROR: falied to create an instance of IPython.Shell.IPShell"
         else :
             try:
                 ipshell = IPython.Shell.IPShell( argv=['-prompt_in1','CASA <\#>: ','-autocall','2','-colors',__ipython_colors, '-nomessages', '-nobanner','-upgrade','-ipythondir',casa['dirs']['rc']+'/ipython'], user_ns=globals() )
-            except: 
+            except e:
+                print e
                 print "ERROR: falied to create an instance of IPython.Shell.IPShell"
     ipshell.IP.runlines('execfile("'+fullpath+'")')
 
